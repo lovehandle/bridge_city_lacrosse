1e3295dbfd9b54c354f12d3ac1877733efceba5d
Ryan Closner <ryan@ryanclosner.com>
2012-11-10 05:40:48 -0800

    Configure deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,13 @@
 set :application, "bridge_city_lacrosse"
-set :repository,  "git@github.com:rclosner/bridge_city_lacrosse"
-set :scm, :git
-set :deploy_to, "/var/www/bridge_city_lacrosse"
 
-set :deploy_via, :remote_cache
-set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
+set :scm, :git
+set :git_enable_submodules, 1
+set :repository,  "git@github.com:rclosner/bridge_city_lacrosse"
+set :branch, "master"
+
+set :user, "deploy"
+set :use_sudo, false
+set :runner, "deploy"
+set :deploy_to, "/var/www/bridge_city_lacrosse"
 
 server "ryanclosner.com", :app
