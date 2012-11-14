51d228712a1b638e1f3f6f134f6997f4d144cce3
Ryan Closner <ryan@ryanclosner.com>
2012-11-14 02:52:09 -0800

    Remove flex slider

--- wp-content/themes/bridge_city_lacrosse/library/bones.php
+++ wp-content/themes/bridge_city_lacrosse/library/bones.php
@@ -270,8 +270,6 @@ function bones_scripts_and_styles() {
 
     // modernizr (without media query polyfill)
     wp_register_script( 'bones-modernizr', get_stylesheet_directory_uri() . '/library/js/libs/modernizr.custom.min.js', array(), '2.5.3', false );
-    // flex slider
-    wp_register_script( 'bones-slider', get_stylesheet_directory_uri() . '/library/js/libs/jquery.flexslider-min.js', array('jquery'),'1.0', true );
 
     // register main stylesheet
     wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
@@ -289,7 +287,6 @@ function bones_scripts_and_styles() {
 
     // enqueue styles and scripts
     wp_enqueue_script( 'bones-modernizr' );
-    wp_enqueue_script( 'bones-slider' );
     wp_enqueue_style( 'bones-stylesheet' );
     wp_enqueue_style('bones-ie-only');
     /*
