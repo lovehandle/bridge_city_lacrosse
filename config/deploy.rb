ffeb47eb9ab531acb89bfe03e409da13764389cc
Ryan Closner <ryan@ryanclosner.com>
2012-11-14 01:03:36 -0800

    chown files after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,17 +7,24 @@ set :branch, "master"
 
 set :user, "deploy"
 set :group, "www-data"
-set :use_sudo, false
+set :use_sudo, true
 set :runner, "deploy"
 set :deploy_to, "/var/www/bridge_city_lacrosse"
 
 server "ryanclosner.com", :app
+
+default_run_options[:pty] = true
 
 namespace :wordpress do
   task :symlink, :roles => :app do
     run "ln -nfs #{shared_path}/uploads #{release_path}/wp-content/uploads"
     run "ln -nfs #{shared_path}/local-config.php #{release_path}/wp-config.php"
   end
+
+  task :priveleges, :roles => :app do
+    run "#{sudo} chown -R www-data:www-data #{release_path}"
+  end
 end
 
 after "deploy:symlink", "wordpress:symlink"
+after "deploy", "wordpress:priveleges"
